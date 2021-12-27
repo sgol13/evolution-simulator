3696c228ad42c24035e7293ff7b5dab391dceb8d
Szymon Golebiowski <szymon13gol@gmail.com>
2021-12-27 16:29:15 +0100

    charts finished

--- app/src/main/java/sgol13/evolution/simulator/gui/QueueChart.java
+++ app/src/main/java/sgol13/evolution/simulator/gui/QueueChart.java
@@ -1,5 +1,6 @@
 package sgol13.evolution.simulator.gui;
 
+import javafx.geometry.Pos;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -26,29 +27,37 @@ public class QueueChart {
         xAxis.setForceZeroInRange(false);
         xAxis.setAnimated(false);
         xAxis.setTickLabelsVisible(false);
+        xAxis.setTickMarkVisible(false);
+        xAxis.setMinorTickVisible(false);
+        xAxis.setVisible(false);
 
         yAxis = new NumberAxis();
+        yAxis.setVisible(false);
         yAxis.setAutoRanging(true);
         yAxis.setAnimated((false));
 
         chart = new LineChart<Number, Number>(xAxis, yAxis);
         chart.setCreateSymbols(false);
         chart.getData().add(dataSeries);
+        chart.setStyle(".default-color0.chart-series-line { -fx-stroke: blue; }");
 
         chartBox.getChildren().add(new Label(label));
         chartBox.getChildren().add(chart);
+        chartBox.setAlignment(Pos.CENTER);
     }
 
     public void update(double value) {
 
-        dataSeries.getData().add(new XYChart.Data<Number, Number>(day, value));
+        dataSeries.getData().add(
+                new XYChart.Data<Number, Number>(day + CHART_DAYS_SPAN, value));
         day++;
 
         if (day > CHART_DAYS_SPAN) {
             dataSeries.getData().remove(0);
-            xAxis.setLowerBound(Math.max(0, day - CHART_DAYS_SPAN));
-            xAxis.setUpperBound(day);
         }
+
+        xAxis.setLowerBound(day);
+        xAxis.setUpperBound(day + CHART_DAYS_SPAN);
 
     }
 
